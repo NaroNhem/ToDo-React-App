02c7b6f540ea40ee36cb915c9d0601403d5806a4
Andrew Sanchez <druu831@gmail.com>
2023-03-14 16:58:21 -0700

    mongodb connection. todo model

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer } = require("apollo-server");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 const typeDefs = require("./schema/schema");
 
 // Mock data
@@ -15,10 +17,16 @@ const mocks = {
 
 const server = new ApolloServer({ typeDefs, mocks });
 
-server.listen().then(() => {
-  console.log(`
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connection successful");
+    return server.listen({ port: 4000 });
+  })
+  .then((res) => {
+    console.log(`
     🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at http://localhost:4000
+    🔉  Listening on port ${res.port}
+    📭  Query at http://localhost:${res.port}
   `);
-});
+  });
